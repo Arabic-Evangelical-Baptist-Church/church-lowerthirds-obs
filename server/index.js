afca5ca1c7bd073605dc9a1002362bbf19b7d00a
Mario Kirolos <mariofayek87@gmail.com>
2023-08-05 12:53:35 +0000

    cleanup repo

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8082 })
+
+wss.on("connection" , ws => {
+    console.log('New Client Connected')
+
+    ws.on("message" , data => {
+        // Broadcast the message to all connected clients (tabs)
+        console.log(`Data Recieved is ${data}`)
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(`${data}`);
+            }
+        });
+
+    })
+
+
+    ws.on("close", () => {
+        console.log('Client left')
+    })
+})
